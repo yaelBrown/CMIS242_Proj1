8bf13a297473db3ce6ce17212a2c29e4a1a4bf8b
Yael Brown <yaelrbrown@gmail.com>
2020-03-15 01:03:07 -0500

    finished: weight controller

--- src/Project1.java
+++ src/Project1.java
@@ -1,23 +1,29 @@
-//The first programming project involves writing a program that computes the minimum, the maximum and the average weight of a collection of weights represented
+// The first programming project involves writing a program that computes the minimum, the maximum and the average weight of a collection of weights represented
 // in pounds and ounces that are read from an input file. This program consists of two classes. The first class is the Weight class, which is specified in integer
-// pounds and ounces stored as a double precision floating point number. It should have five public methods and two private methods
+// pounds and ounces stored as a double precision floating point number. It should have five public methods and two private methods.
+
+import javax.swing.*;
+import java.text.DecimalFormat;
+
 
 public class Project1 {
-    private float weightLbs;
-    private float weightOz;
-
-    public Project1(float weight, boolean isLbs) {
-        if (isLbs) {
-            this.weightLbs = weight;
-        } else {
-            this.weightOz = weight / 16;
-        }
+    private static float findMinium(float[] w) {
+        return 1.0f;
     }
 
-
     public static void main(String[] args) {
-        
+        JFileChooser jfc = new JFileChooser();
 
+        Weight x = new Weight(12.0f, true);
+        System.out.println(x.toString());
+
+        // read in file and add them to an array
+
+        // if file contains more than 25 weights, display error message and terminate
+
+        // sort the array smallest to largest
+
+        // find average
 
     }
 
--- src/Weight.java
+++ src/Weight.java
@@ -1,2 +1,50 @@
+import java.text.DecimalFormat;
+
 public class Weight {
+
+    private float weightLbs = 0.0f;
+    private float weightOz = 0.0f;
+
+    private static DecimalFormat df = new DecimalFormat("0.000");
+
+    public Weight(float weight, boolean isLbs) {
+        if (isLbs) {
+            this.weightLbs = weight;
+            this.weightOz = weight * 16;
+        } else {
+            this.weightLbs = weight / 16;
+            this.weightOz = weight;
+        }
+    }
+
+    public Boolean lessThan(float w) {
+        return w < this.weightLbs;
+    }
+
+    public float addTo(float w) {
+        this.weightLbs = this.weightLbs += w;
+        this.weightOz = this.weightOz += (w * 16);
+        return this.weightLbs;
+    }
+
+    public int divide(int divisor) {
+        return (int) Math.round(weightLbs / divisor);
+    }
+
+    public String toString() {
+        return (this.weightLbs + " lbs " + df.format(this.weightOz));
+    }
+
+    private float toOnces() {
+        return this.weightOz;
+    }
+
+    private float normalize() {
+        if (this.weightLbs > 1) {
+            return 16.0f;
+        } else {
+            return this.weightLbs * 16;
+        }
+    }
+
 }
